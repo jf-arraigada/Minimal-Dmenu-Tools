1a9719100f42759236ccf7b1bf4135eb84bc2a53
focusin <drktheproducer@gmail.com>
2026-03-09 18:05:31 -0300

    Feature: add msg function, and redirect to stdout and install.log file

--- common/common.sh
+++ common/common.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
 log() {
-  printf "\033[1;32m[INFO]\033[0m %s\n" "$*"
+  printf "\033[1;32m[INFO]\033[0m %s\n" "$1"
+  printf "\033[1;32m[INFO]\033[0m %s\n" "$1" >> "$2"
+}
+
+msg() {
+  printf "%s" "$1"
+  [ -n "$2" ] && printf "%s" "$1" >> "$2"
 }
 
 error() {
-  printf "\033[1;31m[ERROR]\033[0m %s\n" "$*" >&2
+  printf "\033[1;31m[ERROR]\033[0m %s\n" "$1" >&2
+  printf "\033[1;31m[ERROR]\033[0m %s\n" "$1" >> "$2"
 }
 
 require() {
   command -v "$1" >/dev/null || {
-    echo "Missing dependency: $1"
+    msg "Missing dependency: $1" "$2"
     exit 1
   }
 }
